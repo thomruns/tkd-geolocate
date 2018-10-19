be0f150457fe4831da1f0279d31dc9d493a2e48e
Thom David <thomdavid74@gmail.com>
2018-10-19 11:55:29 -0700

    Geolocation added

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -27,8 +27,20 @@ export default {
     }
   },
   mounted() {
-    this.renderMap()
-    //console.log(firebase.auth().currentUser)
+    // get user geolocation
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.lat = pos.coords.latitude
+        this.lng = pos.coords.longitude
+        this.renderMap()
+      }, (err) => {
+        console.log(err)
+        this.renderMap()
+      }, { maximumAge: 6000, timeout: 8000 })
+    } else {
+      // position center by default values
+      this.renderMap()
+    }
   }
 }
 </script>
